7b479fb8da0f57b476e19a7469f1e83dbfc9d048
Ting Hsuan Shen <rebecca4122031220@gmail.com>
2024-07-05 11:55:21 +0800

    設置變數的port

--- week-3/myapp/app.js
+++ week-3/myapp/app.js
@@ -3,6 +3,7 @@ const bodyParser= require('body-parser');
 const cookieParser= require('cookie-parser');
 
 const app = express();
+const PORT = process.env.PORT || 3030;
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
@@ -27,6 +28,6 @@ app.use((err, req, res, next) =>{
     res.render('error');
 });
 
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000');
+app.listen(PORT, () => {
+    console.log(`The application is running on localhost:${PORT}`);
 });
