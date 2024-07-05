5212016fe8024b421a6fde92a945a145502a018a
Ting Hsuan Shen <rebecca4122031220@gmail.com>
2024-07-05 13:54:47 +0800

    可以經由變數或是port3000開啟

--- week-3/myapp/app.js
+++ week-3/myapp/app.js
@@ -3,7 +3,7 @@ const bodyParser= require('body-parser');
 const cookieParser= require('cookie-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
