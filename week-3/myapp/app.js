ee01425ed84c4bfa3802d2c9bc1d3a5620255f53
Ting Hsuan Shen <rebecca4122031220@gmail.com>
2024-07-08 09:35:34 +0800

    format with prettier

--- week-3/myapp/app.js
+++ week-3/myapp/app.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const bodyParser= require('body-parser');
-const cookieParser= require('cookie-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));  
+app.use(express.static("public"));
 
-app.set('view engine', 'pug'); //設定模板template
+app.set("view engine", "pug"); //設定模板template
 
-const mainRoutes = require('./routes');  //./routes.index.pug
+const mainRoutes = require("./routes"); //./routes.index.pug
 app.use(mainRoutes);
 
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
-app.use((err, req, res, next) =>{
-    res.locals.error = err;
-    if (err.status) {
-        res.status(err.status);
-    }
-    res.render('error');
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  if (err.status) {
+    res.status(err.status);
+  }
+  res.render("error");
 });
 
 app.listen(PORT, () => {
-    console.log(`The application is running on localhost:${PORT}`);
+  console.log(`The application is running on localhost:${PORT}`);
 });
--- week-3/myapp/routes/index.js
+++ week-3/myapp/routes/index.js
@@ -1,44 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello, My Server!');
+router.get("/", (req, res) => {
+  res.send("Hello, My Server!");
 });
 
-router.get('/getData', (req, res) => {
+router.get("/getData", (req, res) => {
   const data = req.query.number;
-  let result = '';
-  if(!data){
-    result = 'Lack of Parameter';
-  }else if(isNaN(data)){
-    result = 'Wrong Parameter';
-  }else{
-    let sum =0;
-    for( let i=1; i<=data; i++){
-      sum+=i;
+  let result = "";
+  if (!data) {
+    result = "Lack of Parameter";
+  } else if (isNaN(data)) {
+    result = "Wrong Parameter";
+  } else {
+    let sum = 0;
+    for (let i = 1; i <= data; i++) {
+      sum += i;
     }
-    result =sum;
+    result = sum;
   }
-    res.send(result.toString());
+  res.send(result.toString());
 });
 
-router.get('/myName', (req, res) => {
+router.get("/myName", (req, res) => {
   const name = req.cookies.username;
-  if(name){
+  if (name) {
     res.send(name);
-  }else{
-    res.redirect('/trackname');
+  } else {
+    res.redirect("/trackname");
   }
 });
 
-router.get('/trackname', (req, res) => {
+router.get("/trackname", (req, res) => {
   const name = req.query.name;
-  if(name){
-    res.cookie('username', name);
+  if (name) {
+    res.cookie("username", name);
     res.redirect(`/myName`);
-  }else{
-    res.render('trackname');
+  } else {
+    res.render("trackname");
   }
 });
 
-module.exports = router;  
+module.exports = router;
